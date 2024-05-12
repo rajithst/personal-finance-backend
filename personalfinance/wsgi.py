ede19e6041e593455cdaa51a88860e202541665e
rajithst <rajiththennakoonshady@gmail.com>
2024-05-12 22:22:40 +0900

    update configs to deploy to azure

--- personalfinance/wsgi.py
+++ personalfinance/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-settings_module = 'personalfinance.settings.prod' if 'PRODUCTION' in os.environ else 'apersonalfinance.settings.dev'
+settings_module = 'personalfinance.settings.prod' if 'PRODUCTION' in os.environ else 'personalfinance.settings.dev'
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 application = get_wsgi_application()
