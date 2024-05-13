132c10f61a3b42cfef37418787a997ae7dd383be
rajithst <rajiththennakoonshady@gmail.com>
2024-05-13 23:29:34 +0900

    customize dev db connection with prod to run migrations

--- personalfinance/wsgi.py
+++ personalfinance/wsgi.py
@@ -11,8 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 print('WSGI', os.environ)
-c1 = 'PRODUCTION' in os.environ
-c2 = os.environ['PRODUCTION']
-settings_module = 'personalfinance.settings.prod' if c1 and c2 else 'personalfinance.settings.dev'
+c1 = 'PRODUCTION' in os.environ and os.environ.get('PRODUCTION')
+settings_module = 'personalfinance.settings.prod' if c1 else 'personalfinance.settings.dev'
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 application = get_wsgi_application()
