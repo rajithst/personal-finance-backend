fa0f5c7e670057d4f7c55c7a86a4c01a60ecd4b5
rajithst <rajiththennakoonshady@gmail.com>
2024-05-29 00:37:29 +0900

    catch errors on reading files  from gcs

--- utils/gcs.py
+++ utils/gcs.py
@@ -10,12 +10,9 @@ from google.cloud import storage
 class GCSHandler:
     def __init__(self):
         self.gcs_credentials = os.getenv("GCS_API_KEY", None)
-        print('gcs credentials: ', self.gcs_credentials)
-        print(type(self.gcs_credentials))
         if not self.gcs_credentials:
             raise EnvironmentError(f"GCS_API_KEY not set")
         json_credentials = json.loads(self.gcs_credentials)
-        print('json credentials: ', self.gcs_credentials)
         self.client = storage.Client.from_service_account_info(info=json_credentials)
 
     def upload_file(self, bucket_name, source_file_name, destination_blob_name):
@@ -59,5 +56,5 @@ class GCSHandler:
             data = blob.download_as_string()
             return BytesIO(data)
         except Exception as e:
-            logging.error(e)
+            logging.exception('Error downloading file from bucket')
 
