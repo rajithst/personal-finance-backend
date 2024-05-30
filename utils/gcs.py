2fb76976f80f4e8db740656424133bfc9c781b65
rajithst <rajiththennakoonshady@gmail.com>
2024-05-30 23:01:31 +0900

    bug fixes in data importers

--- utils/gcs.py
+++ utils/gcs.py
@@ -20,14 +20,23 @@ class GCSHandler:
         bucket = self.client.bucket(bucket_name)
         blob = bucket.blob(destination_blob_name)
         blob.upload_from_filename(source_file_name)
-        print(f"File {source_file_name} uploaded to {destination_blob_name}.")
+        logging.info(f"File {source_file_name} uploaded to {destination_blob_name}.")
+
+    def move_blob(self, bucket_name, source_file_name, destination_file_name):
+        """Move a blob from one directory to another within the same bucket."""
+        bucket = self.client.bucket(bucket_name)
+        source_blob = bucket.blob(source_file_name)
+        destination_blob = bucket.blob(destination_file_name)
+        source_blob.copy_to(destination_blob)
+        source_blob.delete()
+        logging.info(f'File {source_file_name} moved to {destination_file_name}.')
 
     def download_file(self, bucket_name, source_blob_name, destination_file_name):
         """Downloads a file from the specified bucket."""
         bucket = self.client.bucket(bucket_name)
         blob = bucket.blob(source_blob_name)
         blob.download_to_filename(destination_file_name)
-        print(f"File {source_blob_name} downloaded to {destination_file_name}.")
+        logging.info(f"File {source_blob_name} downloaded to {destination_file_name}.")
 
     def list_files(self, bucket_name, prefix=None):
         """Lists files in the specified bucket."""
@@ -45,14 +54,17 @@ class GCSHandler:
         bucket = self.client.bucket(bucket_name)
         blob = bucket.blob(file_name)
         blob.delete()
-        print(f"File {file_name} deleted.")
+        logging.info(f"File {file_name} deleted.")
 
-    def get_blob(self, bucket_name, file_name):
-        logging.info(f"Downloading file from bucket {file_name}")
+    def get_blob(self, bucket_name, file_name, move_to_completed=True):
         try:
             bucket = self.client.bucket(bucket_name)
             blob = bucket.blob(file_name)
             data = blob.download_as_string()
+            if move_to_completed:
+                destination_file = 'processed/' + file_name
+                self.move_blob(file_name, destination_file)
+            logging.info(f"Downloaded file from bucket {file_name}")
             return BytesIO(data)
         except Exception as e:
             logging.exception('Error downloading file from bucket')
