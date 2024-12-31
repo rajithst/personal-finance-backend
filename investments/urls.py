c6a09c308e96174fa0c6172fd63869d9e94b1087
rajithst <rajiththennakoonshady@gmail.com>
2024-12-31 16:38:23 +0900

    Add portfolio API to urls

--- investments/urls.py
+++ investments/urls.py
@@ -7,12 +7,12 @@ from investments.apis.forex_api import DailyForexValueDaemonView
 from investments.apis.holding_api import HoldingView
 from investments.apis.stock_api import StockPriceHistoryView, DailyStockValueDaemonView, BulkStockValueUpdaterView
 from investments.apis.stock_purchase_api import StockPurchaseHistoryView, StockPurchaseImportView
-from investments.apis.views import InvestmentPerformanceView, ClientSettingsView
+from investments.apis.views import InvestmentPerformanceView, ClientSettingsView, PortfolioSettingsView
 
 cron_endpoints = [
-    path('dividend/cron/payments/daily/', DividendPaymentDaemonView.as_view()),
+    path('dividends/cron/payments/daily/', DividendPaymentDaemonView.as_view()),
     path('dividends/cron/income/daily/', DividendIncomeDaemonView.as_view()),
-    path('stocks/cron/value/daily', DailyStockValueDaemonView.as_view()),
+    path('stocks/cron/value/daily/', DailyStockValueDaemonView.as_view()),
     path('forex/cron/value/daily/', DailyForexValueDaemonView.as_view()),
 ]
 
@@ -32,6 +32,7 @@ urlpatterns = [
     path('stocks/price/history/', StockPriceHistoryView.as_view()),
     path('company/list/', CompanyListView.as_view()),
     path('stocks/purchase/history/', StockPurchaseHistoryView.as_view()),
+    path('portfolio/', PortfolioSettingsView.as_view()),
     path('settings/', ClientSettingsView.as_view())
 
 
